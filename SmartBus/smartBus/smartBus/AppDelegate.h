ce2bec3cbd49375f6cad33ac756e5992cf86adda
Eda <i@eda.im>
2017-03-16 19:52:32 +0800

    add server and iOS

---
+++ SmartBus/smartBus/smartBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  smartBus
+//
+//  Created by Eda on 2017/3/16.
+//  Copyright © 2017年 Eda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartBus/smartBus/smartBus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  smartBus
+//
+//  Created by Eda on 2017/3/16.
+//  Copyright © 2017年 Eda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
